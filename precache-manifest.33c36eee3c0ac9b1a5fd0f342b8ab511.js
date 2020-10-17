d073566ee6ba8170d0e7633fbd2c5596425d0515
harshanmo <harshan.morawaka@qualitapps.com>
2020-10-17 20:31:15 +0530

    build issue

---
+++ precache-manifest.33c36eee3c0ac9b1a5fd0f342b8ab511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f5cbbe6186383c8f5b2173e7a70311",
+    "url": "/temperapp/index.html"
+  },
+  {
+    "revision": "812446acecfc1caf8396",
+    "url": "/temperapp/static/css/main.821b6b58.chunk.css"
+  },
+  {
+    "revision": "054e25acf72b535ca41e",
+    "url": "/temperapp/static/js/2.b068643b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/temperapp/static/js/2.b068643b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "812446acecfc1caf8396",
+    "url": "/temperapp/static/js/main.810f8bcb.chunk.js"
+  },
+  {
+    "revision": "c29959a156713fd19377",
+    "url": "/temperapp/static/js/runtime-main.c64604e3.js"
+  }
+]);
